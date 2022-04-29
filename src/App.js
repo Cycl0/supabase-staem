d53ae14ebdb198e2778a78e853e704d5531cff48
Cycl0 <lucas.cyra@etec.gov.sp.br>
2022-04-29 19:16:59 -0300

    carousel implemented

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
 import {
   AppShell,
   Header,
@@ -6,25 +8,77 @@ import {
   Text,
   Title,
   useMantineTheme,
+  LoadingOverlay,
 } from "@mantine/core";
 import { ReactComponent as InstallIcon } from "./assets/installIcon.svg";
+import Carousel from "./components/Carousel/carousel";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [carouselGames, setCarouselGames] = useState([]);
+  const [loading, setLoading] = useState(false);
   const theme = useMantineTheme();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+
+      let { data, error, status } = await supabase.from("steam").select("*");
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data.length) {
+        setData(data);
+        getRandomGames(data, 6);
+        console.log(data);
+      }
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getRandomGames = (games, numGames) => {
+    let uniqueIndexList = [];
+    for (let i = 0, l = games.length; i < l; ++i) {
+      uniqueIndexList.push(i);
+    }
+
+    let listRandomGames = [];
+    for (let i = 0; i < numGames; ++i) {
+      const randomIndex = Math.floor(Math.random() * uniqueIndexList.length);
+      const uniqueRandomIndex = uniqueIndexList.splice(randomIndex, 1);
+      listRandomGames.push(games[uniqueRandomIndex]);
+    }
+    setCarouselGames(listRandomGames);
+  };
+
   return (
     <AppShell
       padding="md"
       header={
         <Header
-          height={100}
+          height={160}
           pt="xs"
           px="xl"
           sx={(theme) => ({
-            backgroundColor: theme.colors.primary[6],
+            background: "transparent",
             border: "none",
           })}
         >
-          <Grid justify="space-between" p="xl" style={{ height: "100%" }}>
+          <Grid
+            justify="space-between"
+            align="center"
+            p="xl"
+            style={{ height: "100%" }}
+          >
             <Title
               style={{ fontWeight: "700", fontSize: "40px", color: "white" }}
             >
@@ -34,7 +88,7 @@ function App() {
               radius="xl"
               size="md"
               sx={(theme) => ({
-                backgroundColor: theme.colors.primary[5],
+                backgroundColor: `${theme.colors.primary[5]} !important`,
                 padding: "0 40px",
               })}
             >
@@ -45,13 +99,25 @@ function App() {
         </Header>
       }
       sx={(theme) => ({
-        width: "100%",
+        maxWidth: "80%",
+        margin: "0 auto",
         minHeight: "100vh",
         paddingBottom: "140px",
-        backgroundColor: theme.colors.primary[6],
       })}
-    ></AppShell>
+    >
+      <LoadingOverlay
+        loaderProps={{
+          size: "xl",
+          color: theme.colors.primary[12],
+          variant: "bars",
+        }}
+        overlayOpacity={0.1}
+        overlayColor={theme.colors.primary[12]}
+        visible={loading}
+      />
+      <Carousel games={carouselGames} />
+    </AppShell>
   );
-}
+};
 
 export default App;
