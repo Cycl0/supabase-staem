d2c455d728ab8d91e2e46ca0634b691742cee0c9
Cycl0 <lucas.cyra@etec.gov.sp.br>
2022-05-09 01:01:38 -0300

    changed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ module.exports = {
   theme: {
     screens: {
       xs: "350px",
-      sm: "600px",
-      md: "960px",
-      lg: "1280px",
-      xl: "1920px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      2xl: "1536px",
     },
     extend: {},
   },
